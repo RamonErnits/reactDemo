492eb4bba6a00f4291c31cd059b5bad4f860da2f
Opilane TTHK <opilane@tthk.ee>
2022-11-07 16:36:21 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -2,28 +2,44 @@ import Navbar from "./Navbar"
 import Contact from "./pages/Contact"
 import About from "./pages/About"
 import Home from "./pages/Home"
+import {Route, Routes} from "react-router-dom"
 
 function App() {
-  let component
-  switch (window.location.pathname){
-    case "/" :
-      component = <Home />
-      break
-      case "/contact" :
-      component = <Contact />
-      break
-      case "/about" : 
-      component = <About />
-      break
-  }
   return (
-  <>
-  <Navbar />
-  <div className="container">
-    {component}
-  </div>
-  </>
+    <>
+    <Navbar/>
+    <div className="container">
+      <Routes>
+         <Route path="/" element={<Home/>} />
+         <Route path="/about" element={<About/>} />
+         <Route path="/contact" element={<Contact/>} />
+      </Routes>
+   
+    </div>
+    </>
   )
+
+  // let component
+  // switch (window.location.pathname){
+  //   case "/" :
+  //     component = <Home />
+  //     break
+  //     case "/contact" :
+  //     component = <Contact />
+  //     break
+  //     case "/about" : 
+  //     component = <About />
+  //     break
+  // }
+  // return (
+  // <>
+  // <Navbar />
+  // <div className="container">
+  //   {component}
+  // </div>
+  // </>
+  // )
 }
+
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,26 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
 export default function Navbar() {
     return (
         <nav className="nav">
-            <a href="/" className="site-title">Site Name</a>
-            <ul>
-                <li>                
-                    <a href="/about">About</a>
-                </li>
-                <li>                
-                    <a href="/contact">Contact</a>
-                </li>
+            <Link to="/" className="site-title">Site Name</Link>
+            <ul>     
+                    <CustomLink to="/about">About</CustomLink>
+                  
+                    <CustomLink to="/contact">Contact</CustomLink>
             </ul>
         </nav>
+    )
+}
+
+function CustomLink({ to, children, ...props}) {
+  const resolvePath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvePath.pathname, end: true})
+    return (
+    <li className={isActive ? "active" : ""}>
+        <Link to={to} {...props}>
+            {children}
+        </Link>
+    </li>
     )
 }

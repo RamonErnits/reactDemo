965118625d25830afb2661732a263c176b3f7f57
Opilane TTHK <opilane@tthk.ee>
2022-11-07 15:55:46 +0200

    First Commit (Navbar)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+import Home from "./pages/Home"
 
 function App() {
+  let component
+  switch (window.location.pathname){
+    case "/" :
+      component = <Home />
+      break
+      case "/contact" :
+      component = <Contact />
+      break
+      case "/about" : 
+      component = <About />
+      break
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  <Navbar />
+  <div className="container">
+    {component}
+  </div>
+  </>
+  )
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <a href="/" className="site-title">Site Name</a>
+            <ul>
+                <li>                
+                    <a href="/about">About</a>
+                </li>
+                <li>                
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
